b56cabc2d8c4cf5039feed9f4fac2bac12ae68f0
Jieun Hwang <hwangje36@gmail.com>
2023-12-07 13:35:41 +0900

    artillery 부하테스트 추가

--- app/app.ts
+++ app/app.ts
@@ -14,6 +14,17 @@ export const createApp = (client: RedisClient) => {
         response.status(200).send("hello from express, deployed on AWS Lightsail and GitHub Action with solo workflow. (background)")
     });
 
+    function fibonacci(n: number): number {
+        if (n <= 2) return 1;
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    app.get("/fibonacci/:n", (req, res) => {
+        const n = parseInt(req.params.n, 10);
+        const result = fibonacci(n);
+        res.send(`Fibonacci(${n}) = ${result}`)
+    });
+
     app.post("/messages", async (request, response) => {
         const { message } = request.body;
         await client.lPush(LIST_KEY, message);    // await여야 하는 이유: redis의 작업이 끝난 후 app이 결과를 받아야 하기 때문!
